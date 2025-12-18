20c02107e5cae6606b0b6f0f9d2665d215ed684c
senor-23 <nakasena.2312@gmail.com>
2025-12-18 16:10:17 +0900

    Update README.py

--- README.py
+++ README.py
@@ -2,3 +2,9 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
+
+url=https://github.com/kouki20004/suisen_sisutem-/edit/main/README.py
+real2=https://cckyotosuacjp-my.sharepoint.com/:x:/g/personal/g2303798_cc_kyoto-su_ac_jp/IQCra9twLTANTZ13fy0KtZqTAb2K26OT9z68S-x-yyQAdKo?e=cw4LVa
+
+st.title("京産大学部マッチング")
+st.write("以下の質問に回答してください")
